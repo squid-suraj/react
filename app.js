9254a705466973965786b51a99d820a1cf5b8d1f
Suraj Kumar <suraj.20227053@mnnit.ac.in>
2025-09-02 04:13:24 +0530

    episode -01

---
+++ app.js
@@ -0,0 +1,21 @@
+// const heading = React.createElement(
+//     "h1", 
+//     { id : "heading", xyz : "abc"}, 
+//     "Hellow world from react!");
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+
+const parent = React.createElement("div", {id : "parent"}, [
+    React.createElement("div", {id : "child"}, [
+        React.createElement("h1", {}, "i am h1 tag"),
+        React.createElement("h2", {}, "i am h2 tag"),
+    ]),
+    React.createElement("div", {id : "i am child2"}, [
+        React.createElement("h1", {}, "i am h1 tag"),
+        React.createElement("h2", {}, "i am h2 tag"),
+    ]),
+]);
+
